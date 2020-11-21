dc9256155b0ae1046b91940b3622dcf91c006ea2
charu93 <charmila0409@gmail.com>
2020-11-21 16:11:08 +0530

    Task3.1

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,36 @@
+package org.emp;
+
+public class Employee {
+	
+	//empId(),empName(),empDob(),empPhone(),empEmail(),empAddress()
+public void empId() {
+	System.out.println("Employee ID is 2000");
+}
+public void empName() {
+	System.out.println("Employee Name is Charu");
+	
+}
+public void empDob() {
+	System.out.println("Emp Dob: 04 Jan 1994 ");
+}
+public void empPhone() {
+	System.out.println("Employee Phone num is 7708583705");
+}
+public void empEmail() {
+	System.out.println("Emp email id is charmila0409@gmail.com");
+	
+}
+public void empAddress() {
+	System.out.println("Employee Address is Bangalore");
+}
+public static void main(String[] args) {
+	Employee e = new Employee();
+	e.empAddress();
+	e.empDob();
+	e.empEmail();
+	e.empId();
+	e.empName();
+	e.empPhone();
+}
+}
+
